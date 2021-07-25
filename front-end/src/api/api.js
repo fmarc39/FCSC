08b9bce069dc7ecab783ead954c31880fa0fcd1f
Marc François <fmarc@MacBook-Pro-de-Marc.local>
2021-07-25 22:06:18 +0200

    v1

--- front-end/src/api/api.js
+++ front-end/src/api/api.js
@@ -2,8 +2,7 @@ import axios from 'axios';
 import { authHeader } from 'selectors/auth-header';
 
 export default axios.create({
-  baseURL: 'http://localhost:3001',
-  //baseURL: 'https://fcsc-back.herokuapp.com/',
+  baseURL: 'https://fcsc-back.herokuapp.com/',
   timeout: 5000,
   // headers: authHeader(),
 });
